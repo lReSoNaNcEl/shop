bc71c7c98cd9d7763d4f0f0293bab3146137c64e
Ivan <iresonancei@mail.com>
2020-06-18 13:54:26 +0300

    'Save'

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -3,22 +3,22 @@
         <Header/>
         <section class="catalog">
             <h1 class="catalog__title title">Каталог</h1>
-            <div class="catalog__wrapper">
-                <div class="catalog__filter">
-                    <h2 class="filter__title">Фильтр</h2>
-                    <div class="filter__condition" v-for="filter in filters">
-                        <div class="condition__type" @click="renderFilterParams" :data-id="filter.id">
-                            <p class="type__text">{{filter.type}}</p>
-                            <img class="type__icon" src="img/arrow_icon.svg"/>
-                        </div>
-                        <div class="condition__params">
-                            <div class="params__item" v-for="param in filter.params">
-                                <input class="params__state" type="checkbox">
-                                <div class="params__name">{{param.name}}</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<!--            <div class="catalog__wrapper">-->
+<!--                <div class="catalog__filter">-->
+<!--                    <h2 class="filter__title">Фильтр</h2>-->
+<!--                    <div class="filter__condition" v-for="filter in filters">-->
+<!--                        <div class="condition__type" @click="renderFilterParams" :data-id="filter.id">-->
+<!--                            <p class="type__text">{{filter.type}}</p>-->
+<!--                            <img class="type__icon" src="img/arrow_icon.svg"/>-->
+<!--                        </div>-->
+<!--                        <div class="condition__params">-->
+<!--                            <div class="params__item" v-for="param in filter.params">-->
+<!--                                <input class="params__state" type="checkbox">-->
+<!--                                <div class="params__name">{{param.name}}</div>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
                 <div class="catalog__products">
                     <nuxt-link class="products__basket" to="/shop/basket">
                         <img class="basket__img" src="img/basket.svg"/>
@@ -27,7 +27,7 @@
                         <Product v-for="product in products" :id="product.id" :title="product.title" :img="product.img" :desc="product.desc" :price="product.price" :volume="product.volume"/>
                     </div>
                 </div>
-            </div>
+<!--            </div>-->
         </section>
         <Footer/>
     </div>
@@ -43,21 +43,21 @@ export default {
         filters() {return this.$store.getters['filter/getFilters']},
     },
     methods: {
-        renderFilterParams(e) {
-            const filterParams = e.currentTarget.nextElementSibling
-            const icon = e.currentTarget.lastChild
-
-            setTimeout(() => filterParams.classList.contains('show') ? filterParams.style.display = 'block' : filterParams.style.display = 'none', 300)
-
-            if (filterParams.classList.toggle('show')) {
-                icon.style.transform = 'rotate(0deg)'
-                filterParams.style.animation = '.6s Show'
-            }
-            else {
-                icon.style.transform = 'rotate(180deg)'
-                filterParams.style.animation = '.6s Hide'
-            }
-        }
+        // renderFilterParams(e) {
+        //     const filterParams = e.currentTarget.nextElementSibling
+        //     const icon = e.currentTarget.lastChild
+        //
+        //     setTimeout(() => filterParams.classList.contains('show') ? filterParams.style.display = 'block' : filterParams.style.display = 'none', 300)
+        //
+        //     if (filterParams.classList.toggle('show')) {
+        //         icon.style.transform = 'rotate(0deg)'
+        //         filterParams.style.animation = '.6s Show'
+        //     }
+        //     else {
+        //         icon.style.transform = 'rotate(180deg)'
+        //         filterParams.style.animation = '.6s Hide'
+        //     }
+        // }
     },
     components: {Header, Footer, Product}
 }
