c6af77a4d4a04519019dc43108d3d7a6e4b85f4a
Resonance <iresonancei@mail.ru>
2020-06-26 21:33:42 +0300

    'Save'

--- pages/shop/product/_id.vue
+++ pages/shop/product/_id.vue
@@ -1,18 +1,20 @@
 <template>
     <div>
         <Header/>
-        <section class="product">
-            <img :src="product.img" class="product__img"/>
-            <h1 class="product__title title">{{product.title}}</h1>
-            <p class="product__desc">{{product.desc}}</p>
-            <div class="product__properties">
+        <section v-show="isRender" class="productfull">
+            <h1 class="productfull__title title">{{product.title}}</h1>
+            <p v-html="product.desc" class="productfull__desc"></p>
+            <div class="productfull__properties">
                 Ширина листа : 1 175 мм<br>
                 Монтажная ширина : 1 080 мм<br>
                 Толщина листа : 0,35 - 0,5 мм
             </div>
-            <p class="product__advantage">Каскад обладает замечательной эстетикой, большим запасом прочности и может устанавливаться на любую кровлю.</p>
-            <p class="product__price">Цена: <span class="highlight">{{product.price}}</span> руб./кв.м</p>
+            <p class="productfull__advantage">Каскад обладает замечательной эстетикой, большим запасом прочности и может устанавливаться на любую кровлю.</p>
+            <p class="productfull__price">Цена: <span class="highlight">{{product.price}}</span> руб./кв.м</p>
+            <img :src="product.image" class="productfull__img"/>
         </section>
+
+        <div v-show="!isRender" :style="{margin: '3vw 0 0 3%', fontSize: '2vw', minHeight: '35vw'}">Запрашиваемый товар не найден</div>
         <Footer/>
     </div>
 </template>
@@ -22,28 +24,35 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 export default {
     data: () => ({
+        isRender: true,
         id: null,
-        product: null
+        product: {
+            title: 'Test',
+            desc: 'Test',
+            price: '300',
+            image: '/img/product/1.png',
+        }
     }),
-    computed: {
-        products() {return this.$store.getters['product/getProducts']},
-    },
     created() {
-        this.id = this.$route.params.id
-        this.product = this.products.find(product => +product.id === +this.id)
+        this.id = +this.$route.params.id
+        this.$store.dispatch('product/saveProduct', this.id).then(() => {
+            this.product = this.$store.getters['product/getProduct']
+            if ('title' in this.product) {}
+            else this.isRender = false
+        })
     },
     components: {Header, Footer}
 }
 </script>
 
 <style scoped>
-    .product {
+    .productfull {
         width: 32%;
-        height: 38vw;
+        min-height: 30vw;
         padding: 0 0 0 3%;
     }
 
-    .product__img {
+    .productfull__img {
         position: absolute;
         width: 59.375vw;
         height: 100vh;
@@ -52,17 +61,18 @@ export default {
         right: 0;
         background-size: 100% 100%;
         clip-path: polygon(0 62%, 24% 0, 100% 0, 100% 100%, 0 100%, 0 75%, 0 72%, 0 68%, 0 65%);
+        filter: grayscale(90%);
     }
 
-    .product__desc,
-    .product__properties,
-    .product__advantage {
+    .productfull__desc,
+    .productfull__properties,
+    .productfull__advantage {
         font-size: .9375vw;
         margin: 0 0 2.083vw 0;
         line-height: 180%;
     }
 
-    .product__price {
+    .productfull__price {
         font-size: 1.14583vw;
     }
 </style>
--- store/product.js
+++ store/product.js
@@ -1,19 +1,29 @@
 export const state = () => ({
-    products: [
-        {id: 1, img: '/img/product/1.png', title: 'Product 1', desc: 'Металлочерепица Каскад произведена из высококачественной стали, проста в обслуживании и чрезвычайно долговечна.', price: 325, volume: 46},
-        {id: 2, img: '/img/product/2.png', title: 'Product 2', desc: 'Супермонтеррей может использоваться в регионах с большим количеством зимних осадков.', price: 200, volume: 31},
-        {id: 3, img: '/img/product/3.png', title: 'Product 3', desc: 'Внешне она очень похожа на керамику. При этом ее конструкция позволяет выдерживать большие нагрузки.', price: 150, volume: 63},
-        {id: 4, img: '/img/product/4.png', title: 'Product 4', desc: 'Металлочерепица Каскад произведена из высококачественной стали, проста в обслуживании и чрезвычайно долговечна.', price: 170, volume: 42},
-        {id: 5, img: '/img/product/5.png', title: 'Product 5', desc: 'Супермонтеррей может использоваться в регионах с большим количеством зимних осадков.', price: 125, volume: 64},
-        {id: 6, img: '/img/product/6.png', title: 'Product 6', desc: 'Внешне она очень похожа на керамику. При этом ее конструкция позволяет выдерживать большие нагрузки.', price: 610, volume: 45},
-        {id: 7, img: '/img/product/7.png', title: 'Product 7', desc: 'Металлочерепица Каскад произведена из высококачественной стали, проста в обслуживании и чрезвычайно долговечна.', price: 155, volume: 13},
-        {id: 8, img: '/img/product/8.png', title: 'Product 8', desc: 'Металлочерепица Каскад произведена из высококачественной стали, проста в обслуживании и чрезвычайно долговечна.', price: 190, volume: 45}
-    ],
+    // products: [
+    //     {id: 1, img: '/img/product/1.png', title: 'Product 1', desc: 'Металлочерепица Каскад произведена из высококачественной стали, проста в обслуживании и чрезвычайно долговечна.', price: 325, volume: 46},
+    //     {id: 2, img: '/img/product/2.png', title: 'Product 2', desc: 'Супермонтеррей может использоваться в регионах с большим количеством зимних осадков.', price: 200, volume: 31},
+    //     {id: 3, img: '/img/product/3.png', title: 'Product 3', desc: 'Внешне она очень похожа на керамику. При этом ее конструкция позволяет выдерживать большие нагрузки.', price: 150, volume: 63},
+    //     {id: 4, img: '/img/product/4.png', title: 'Product 4', desc: 'Металлочерепица Каскад произведена из высококачественной стали, проста в обслуживании и чрезвычайно долговечна.', price: 170, volume: 42},
+    //     {id: 5, img: '/img/product/5.png', title: 'Product 5', desc: 'Супермонтеррей может использоваться в регионах с большим количеством зимних осадков.', price: 125, volume: 64},
+    //     {id: 6, img: '/img/product/6.png', title: 'Product 6', desc: 'Внешне она очень похожа на керамику. При этом ее конструкция позволяет выдерживать большие нагрузки.', price: 610, volume: 45},
+    //     {id: 7, img: '/img/product/7.png', title: 'Product 7', desc: 'Металлочерепица Каскад произведена из высококачественной стали, проста в обслуживании и чрезвычайно долговечна.', price: 155, volume: 13},
+    //     {id: 8, img: '/img/product/8.png', title: 'Product 8', desc: 'Металлочерепица Каскад произведена из высококачественной стали, проста в обслуживании и чрезвычайно долговечна.', price: 190, volume: 45}
+    // ],
     product: null
 })
 
+export const actions = {
+    saveProduct: async (ctx, id) => {
+        const data = await fetch(`https://backsteelhouse.ru/product/${id}/`)
+        const product = await data.json()
+        ctx.commit('setProduct', await product)
+    }
+}
+
+export const mutations = {
+    setProduct: (state, payload) => state.product = payload
+}
 
 export const getters = {
-    getProducts: state => state.products,
     getProduct: state => state.product
 }
