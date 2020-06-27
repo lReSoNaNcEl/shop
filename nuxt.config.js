5e4753856eeb3ede890b10f91a3f19dc1522428d
Resonance <iresonancei@mail.ru>
2020-06-27 19:13:19 +0300

    'Save'

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
         meta: [
             {charset: 'utf-8'},
             {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+            {hid: 'description', name: 'description', content: process.env.npm_package_description || ''},
+            {name: 'keywords', content: 'Магазин кровли, Кровельные материалы в Липецке, купить кровельные материалы в липецке, купить кровлю в липецке, материалы для кровли в липецке, магазин кровельных материалов Липецке, интернет-магазин Steel house, кровельный магазин Липецк, кровельная компания, продукция для кровли'}
         ],
         link: [
             {rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico'},
